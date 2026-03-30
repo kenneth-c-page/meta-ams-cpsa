177161fa4c4271a4bab0d6424d26be73e29d4bfb
Kenneth Page <kennethpage@gideonihah-hq-2.local>
2026-03-30 16:17:56 -0600

    Restructured registrar registration, need to add all mib info and gen-st

--- registrar-registration/registrarRegistration.scm
+++ registrar-registration/registrarRegistration.scm
@@ -1,76 +1,150 @@
 (herald "Meta-AMS (MAMS) Registrar Registration")
+; multiple roles per party
+; each store and load in a different run???
+
+; continuum1:{ venture1:{ unit1:{ cell1, cell2 }}}
+; continuum2:{ venture1:{ unit1:{ cell1, cell2 }}, 
+               venture2:{ unit1.2:{ cell1.2.1, cell1.2.2 }}}
+; continuum3:{ venture2:{ unit1.2:{ cell1.2.1, cell1.2.2 }}}
+
+; unit is an element of a venture, cell is unit intersection continuum
+
+; mpdu structure
+    ; header
+        ; version number
+        ; checksum flag                                             (presence)
+        ; mpdu type                                                 (message type, no need to append message—see supp data below)
+            ; _TYPE (indicated by num)_|_____REFERENCE_____|__SUPP DATA__
+            ; heartbeat                 (heartbeat source)
+            ; rejection                 (echo)              (refusal reason)
+            ; you_are_dead              (0)
+            ; registrar_noted           (echo)
+            ; registrar_unknown         (echo)
+            ; reconnected               (echo)
+            ; announce_registrar        (0)                 (MAMS endpoint name)
+            ; invite                    (module id)         (invitation assertion structure)
+            ; disinvite                 (module id)         (invitation cancellation structure)
+            ; cell_spec                 (echo)              (cell descriptor)
+            ; registrar_query           (query number)      (MAMS endpoint name)
+            ; module_registration       (query number)      (contact summary)
+            ; you_are_in                (echo)              (assigned module number)
+            ; I_am_starting             (module id)         (contact summary)
+            ; I_am_here                 (0)                 (module status list)
+            ; subscribe                 (module id)         (subscription assertion structure)
+            ; unsubscribe               (module id)         (subscription cancellation structure)
+            ; I_am_stopping             (module id)         
+            ; reconnect                 (query number)      (reconnect structure)
+            ; cell_status               (module id)         (module list)
+            ; module_has_started        (module id)         (contact summary)
+            ; I_am_running              (0)
+            ; module_status             (module id)         (module status list)
+        ; sender's venture number                                   (original sender; 0 if conf)
+        ; sender's unit number                                      (original sender; 0 if conf)
+        ; sender's role number                                      (original sender; 0 if reg or conf)
+        ; length of digital signature
+        ; length of supp data
+        ; reference                                                 (ONE of the following, vary by mpdu type)
+            ; query number                                          (sequence, each entity has own series of query numbers)
+            ; echo                                                  (reference number of causally preceding mpdu)
+            ; heartbeat source                                      (module's number or 0)
+            ; module id                                             (above module's number + unit number * 256 + role number * 16777216)
+        ; time tag
+    ; digital signature
+            ; 1) 4 char nonce
+            ; 2) encrypt nonce + public string                      (enc n string (privk))
+                ; module    —role private key
+                ; registrar —application private key
+                ; conf      —continuum private key
+        ; nonce + encrypted string
+    ; supplementary data
+        ; endpoint name                                             (endpoint's name)
+        ; cell descriptor                                           (registrar's unit number+cell's registrar endpoint name)
+        ; module list                                               (length + list of 8-bit module numbers)
+        ; assigned module number                                    (module number assigned to module) by registrar? conf?
+        ; delivery point name                                       (transport service name and delivery endpoint)
+        ; delivery vector                                           (delivery vector id number + number of delivery points + list of delivery point names)
+        ; delivery vector list                                      (length + list of above delivery vectors)
+        ; contact summary                                           (endpoint name + delivery vector list)
+        ; subscription assertion structure                          (subject number + cont number with all subscribed modules + unit number with all subscribed modules + role number for subscribed modules + delivery vector number + priority + flow identifier)
+        ; invitation assertion structure                            (...)
+        ; subscription list                                         (number of subscription assertions + list of assertion structures (identifies subjects subscribing to))
+        ; invitation list                                           (number of invitation assertions + list of assertion structures (not subscribe, but willing to receive))
+        ; declaration structure                                     (subscription list + invitation list)
+        ; module status structure                                   (unit number + module number + originally registered-role number + contact summary + declaration structure)
+        ; module status list                                        (number of module status structures + list of structures)
+        ; subscription cancellation structure                       (subject number + cont number with all subscribed modules + unit number ... + role number ...)
+        ; invitation cancellation structure                         (...)
+        ; reconnect structure                                       (own module status structure + module list)
+        ; refusal reason                                            (numerical code)
+    ; checksum
 
 (defmacro (announce_registrar_mpdu)
     (cat
-        ; mpdu header, per 5.1.1
+        ; header
         (mpdu_header
-            unit_number
-            reg_role_number
-            reg_ref_number
+            venture
+            unit
+            reg_conf_role
+            ar_ref
         )
-        ; mpdu digital signature, per 5.1.4
-        ; app private key used to sign, per 5.1.4.3
+        ; digital signature
         (cat
-            n1
+            ar_nonce
             (enc
-                n1
+                ar_nonce
                 "known_string"
-                (privk app)
+                (privk venture)
             )
         )
-        announce_registrar
+        ; supplementary data
+        reg_endpoint_name
     )
 )
+
 (defmacro (registrar_noted_mpdu)
     (cat
+        ; header
         (mpdu_header
-            unit_number
-            conf_role_number
-            conf_ref_number
+            config
+            config
+            reg_conf_role
+            ar_ref
         )
+        ; digital signature
         (cat
-            n2
+            rn_nonce
             (enc
-                n2
+                rn_nonce
                 "known_string"
                 (privk cont)
             )
         )
-        registrar_noted
+        ; supplementary data
     )
 )
-(defmacro (cell_spec_mpdu n)
+
+(defmacro (cell_spec_mpdu echo unit endpoint nonce)
     (cat
+        ; header
         (mpdu_header
-            unit_number
-            conf_role_number
-            conf_ref_number
+            config
+            config
+            reg_conf_role
+            echo
         )
+        ; digital signature
         (cat
-            n
+            nonce
             (enc
-                n
+                nonce
                 "known_string"
                 (privk cont)
             )
         )
-        cell_spec_body
-    )
-)
-(defmacro (heartbeat_mpdu unit_number role_number ref_number n k)
-    (cat
-        (mpdu_header
-            unit_number
-            role_number
-            ref_number
-        )
+        ; supplementary data
         (cat
-            n
-            (enc
-                n
-                "known_string"
-                (privk k)
-            )
+            unit
+            endpoint
         )
     )
 )
@@ -78,147 +152,74 @@
 (defprotocol mams_registrar_reg basic
     (defrole new_registrar
         (vars
-            (n1 text) (unit_number reg_role_number reg_ref_number text) (announce_registrar text) (app name) (reg_mib locn)
-            (n2 text) (conf_role_number conf_ref_number text) (registrar_noted text) (cont name)
-            (n3 text) (cell_spec_body mesg)
+            (ar_nonce text) (reg_endpoint_name venture unit reg_conf_role name) (ar_ref text)
+            (rn_nonce text) (config cont name)
+            (foreign_endpoint_name foreign_unit name) (cs_nonce1 text)
         )
         (trace
             (send (announce_registrar_mpdu))
             (recv (registrar_noted_mpdu))
-            (recv (cell_spec_mpdu n3))
-            (stor
-                reg_mib
-                cell_spec_body
+            (recv (cell_spec_mpdu 
+                        ar_ref 
+                        foreign_unit 
+                        foreign_endpoint_name 
+                        cs_nonce1
+                  )
+            )
+            (store reg_mib
+                foreign_endpoint_name
+                foreign_unit
             )
         )
     )
-    (defrole config_server
+
+    (defrole config_server_accept
         (vars
-            (n0 text) (app1 name)
-            (foreign_role_numbers foreign_ref_numbers text)
-            (n1 unit_number reg_role_number reg_ref_number text) (announce_registrar text) (app name) (conf_mib locn)
-            (n2 conf_role_number conf_ref_number text) (registrar_noted text) (cont name)
-            (n3 text) (cell_spec_body mesg)
-            (n4 text)
+            (ar_nonce text) (reg_endpoint_name venture unit reg_conf_role name) (ar_ref text)
+            (conf_mib locn)
+            (rn_nonce text) (config cont name)
+            (foreign_endpoint_name foreign_unit name)
+            (cs_nonce1 text)
         )
         (trace
-            ; foreign_registrars heartbeat
-            (recv
-                (heartbeat_mpdu
-                    unit_number
-                    foreign_role_numbers
-                    foreign_ref_numbers
-                    n0
-                    app1
-                )
-            )
             (recv (announce_registrar_mpdu))
             (stor
                 conf_mib
-                announce_registrar
+                reg_endpoint_name
+                unit
             )
             (send (registrar_noted_mpdu))
-            (load
-                conf_mib
-                cell_spec_body
+            (load conf_mib 
+                foreign_unit 
+                foreign_endpoint_name
             )
-            (send (cell_spec_mpdu n3))
-            (send (cell_spec_mpdu n4))
-        )
-    )
-    (defrole foreign_registrars
-        (vars
-            (n0 text) (app1 name)
-            (foreign_role_numbers foreign_ref_numbers text)
-            (unit_number conf_role_number conf_ref_number text) (cont name) (foreign_reg_mib locn)
-            (n4 text) (cell_spec_body mesg)
-        )
-        (trace
-            ; heartbeat
-            (send
-                (heartbeat_mpdu
-                    unit_number
-                    foreign_role_numbers
-                    foreign_ref_numbers
-                    n0
-                    app1
-                )
-            )
-            ; cell_spec
-            (recv (cell_spec_mpdu n4))
-            (stor
-                foreign_reg_mib
-                cell_spec_body
+            (send (cell_spec_mpdu 
+                        ar_ref 
+                        foreign_unit 
+                        foreign_endpoint_name 
+                        cs_nonce1
+                  )
             )
         )
     )
-    (lang
-        (mpdu_header (tuple 3))
-    )
-)
 
-; ========== registrar_noted ========== ;
-(defskeleton mams_registrar_reg
-    (vars
-        (n0 text) (foreign_role_numbers foreign_ref_numbers text) (app1 name) 
-        (n1 text) (unit_number reg_role_number reg_ref_number text) (announce_registrar text) (app name) (reg_mib conf_mib locn)
-        (n2 text) (conf_role_number conf_ref_number text) (registrar_noted text) (cont name)
-        (n3 text) (cell_spec_body mesg)
-        (n4 text) (foreign_reg_mib locn)
+    (defrole config_server_forward
+    ; stor foreign as pre-established
+    ; heartbeats
+    ; 
     )
-    (defstrandmax new_registrar
-        (unit_number unit_number) (reg_role_number reg_role_number) (reg_ref_number reg_ref_number) ; header
-        (n1 n1) (app app) (announce_registrar announce_registrar)                                   ; signature and payload
-        (reg_mib reg_mib)
-        (conf_role_number conf_role_number) (conf_ref_number conf_ref_number)
-        (n2 n2) (cont cont) (registrar_noted registrar_noted)
-        (n3 n3) (cell_spec_body cell_spec_body)
+
+    (defrole config_server_reject
     )
-    (non-orig (privk app))
-    (non-orig (privk cont))
-    (uniq-orig n1)
-)
-(defskeleton mams_registrar_reg
-    (vars
-        (n0 text) (foreign_role_numbers foreign_ref_numbers text) (app1 name) 
-        (n1 text) (unit_number reg_role_number reg_ref_number text) (announce_registrar text) (app name) (reg_mib conf_mib locn)
-        (n2 text) (conf_role_number conf_ref_number text) (registrar_noted text) (cont name)
-        (n3 text) (cell_spec_body mesg)
-        (n4 text) (foreign_reg_mib locn)
+
+    (defrole foreign_registrars
     )
-    (defstrandmax config_server
-        (n0 n0) (app1 app1)
-        (foreign_role_numbers foreign_role_numbers) (foreign_ref_numbers foreign_ref_numbers)
-        (unit_number unit_number) (reg_role_number reg_role_number) (reg_ref_number reg_ref_number) ; header
-        (n1 n1) (app app) (announce_registrar announce_registrar)                                   ; signature and payload
-        (conf_mib conf_mib)
-        (conf_role_number conf_role_number) (conf_ref_number conf_ref_number)
-        (n2 n2) (cont cont) (registrar_noted registrar_noted)
-        (n3 n3) (cell_spec_body cell_spec_body)
-        (n4 n4)
+
+    (lang
+        (mpdu_header (tuple 4))
+        ; venture number (name)
+        ; unit number (name)
+        ; role number (name)
+        ; reference number (per strand)
     )
-    (non-orig (privk app1))
-    (non-orig (privk cont))
-    (non-orig (privk app))
-    (uniq-orig n2)
-    (uniq-orig n3)
-    (uniq-orig n4)
-)
-(defskeleton mams_registrar_reg
-    (vars
-        (n0 text) (foreign_role_numbers foreign_ref_numbers text) (app1 name) 
-        (n1 text) (unit_number reg_role_number reg_ref_number text) (announce_registrar text) (app name) (reg_mib conf_mib locn)
-        (n2 text) (conf_role_number conf_ref_number text) (registrar_noted text) (cont name)
-        (n3 text) (cell_spec_body mesg)
-        (n4 text) (foreign_reg_mib locn)
-    )
-    (defstrandmax foreign_registrars
-        (n0 n0) (app1 app1)
-        (foreign_role_numbers foreign_role_numbers) (foreign_ref_numbers foreign_ref_numbers)
-        (unit_number unit_number) (conf_role_number conf_role_number) (conf_ref_number conf_ref_number) (cont cont) (foreign_reg_mib foreign_reg_mib)
-        (n4 n4) (cell_spec_body cell_spec_body)
-    )
-    (non-orig (privk app1))
-    (non-orig (privk cont))
-    (uniq-orig n0)
 )
